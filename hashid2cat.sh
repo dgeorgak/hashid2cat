e72f1511a6979446c9a2e4b0ab149c09c527aae8
dgeorgak <dimgeorgak@pm.me>
2024-11-23 20:55:35 +0200

    working hashcat. buggy when iterating hashtypes

--- hashid2cat.sh
+++ hashid2cat.sh
@@ -39,7 +39,7 @@ fi
 # Extract hash types in a file and as an output message
 file_name="${input_string:0:10}-hashtypes.txt"
 echo "$output" | grep "\[+\] " | sed 's/^\[+\] //' > "$file_name"
-cat "$file_name"
+# cat "$file_name"
 
 # Parse hashcat manual for "Hash types"
 hashcat_manual=$(man hashcat | sed -n '/^ *Hash types/,/^$/p')
@@ -61,9 +61,45 @@ done < "$file_name"
 
 if [[ $matches_found -eq 0 ]]; then
     echo "No matches found in hashcat for the hash types requested"
-else
-    cat "$hashcat_numbers_file"
+    exit 1
 fi
 
+# TODO: switch to providing wordlist as script input with "-w"
+wordlist="/usr/share/wordlists/rockyou.txt"
+
+if [[ ! -f "$wordlist" ]]; then
+    echo "Error: Wordlist $wordlist not found. Please ensure rockyou.txt is installed and available."
+    exit 1
+fi
+
+# Run hashcat looping through detected hash types
+while read -r mode; do
+    echo "Running hashcat with mode $mode..."
+    hashcat_output=$(hashcat -m "$mode" -a 0 "$input_string" "$wordlist" 2>&1)
+    # echo "$hashcat_output"
+    echo " "
+    if echo "$hashcat_output" | grep -q "Recovered........: 1/1 (100.00%)" || echo "$hashcat_output" | grep -q "INFO: All hashes found as potfile and/or empty entries!"; then
+        echo "Hash successfully cracked with mode $mode:"
+	potfile_output=$(hashcat -m "$mode" -a 0 "$input_string" "$wordlist" --show 2>&1)
+        rm "$hashcat_numbers_file"
+        echo "$potfile_output"
+        exit 0
+    elif echo "$hashcat_output" | grep -q "Recovered........: 0/1 (0.00%)"; then
+        echo "Hash not cracked with mode $mode. Waiting for hashcat to stop..."
+        # Wait for the "Stopped: " message before continuing
+        while true; do
+            hashcat_status=$(hashcat -m "$mode" --status --status-json --quiet 2>/dev/null)
+            if echo "$hashcat_status" | grep -q "Stopped: "; then
+                echo "Hashcat has stopped. Proceeding to the next mode..."
+                break
+            fi
+            sleep 1
+        done
+    fi
+done < "$hashcat_numbers_file"
+
 # TODO:  Uncomment once the script is done
-# rm "$hashcat_numbers_file"
+rm "$hashcat_numbers_file"
+
+echo "Hash not cracked with any of the detected modes."
+exit 0
